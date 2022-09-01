1114b3e24deae86800cc628fc711860d3cbe0cb4
Peter Ivanic <peterivanic89@gmail.com>
2022-09-01 17:55:26 +0200

    override save method in UserServiceImpl.java

--- src/main/java/com/example/registrationloginspringbootsecuritythymeleaf/services/UserServiceImpl.java
+++ src/main/java/com/example/registrationloginspringbootsecuritythymeleaf/services/UserServiceImpl.java
@@ -1,13 +1,28 @@
 package com.example.registrationloginspringbootsecuritythymeleaf.services;
 
 import com.example.registrationloginspringbootsecuritythymeleaf.dtos.UserRegistrationDto;
+import com.example.registrationloginspringbootsecuritythymeleaf.models.Role;
 import com.example.registrationloginspringbootsecuritythymeleaf.models.User;
+import com.example.registrationloginspringbootsecuritythymeleaf.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public User save(UserRegistrationDto userRegistrationDto) {
-        return null;
+        User user = new User(
+                userRegistrationDto.getFirstName(),
+                userRegistrationDto.getLastName(),
+                userRegistrationDto.getEmail(),
+                userRegistrationDto.getPassword(),
+                Arrays.asList(new Role("ROLE_USER")));
+        return userRepository.save(user);
     }
 }
