0163af170c47892eb1fdb66c478b33d6367c9800
Peter Ivanic <peterivanic89@gmail.com>
2022-09-01 17:35:24 +0200

    new User.java and Role.java

---
+++ src/main/java/com/example/registrationloginspringbootsecuritythymeleaf/models/Role.java
@@ -0,0 +1,19 @@
+package com.example.registrationloginspringbootsecuritythymeleaf.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
