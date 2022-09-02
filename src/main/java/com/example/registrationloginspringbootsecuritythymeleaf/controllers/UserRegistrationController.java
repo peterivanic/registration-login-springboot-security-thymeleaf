61f62087fe33ae61def62bbcca814cb58d57b4a0
Peter Ivanic <peterivanic89@gmail.com>
2022-09-02 13:21:54 +0200

    new showReqistratioController and registerUser Account

--- src/main/java/com/example/registrationloginspringbootsecuritythymeleaf/controllers/UserRegistrationController.java
+++ src/main/java/com/example/registrationloginspringbootsecuritythymeleaf/controllers/UserRegistrationController.java
@@ -1,4 +1,28 @@
 package com.example.registrationloginspringbootsecuritythymeleaf.controllers;
 
+import com.example.registrationloginspringbootsecuritythymeleaf.dtos.UserRegistrationDto;
+import com.example.registrationloginspringbootsecuritythymeleaf.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
 public class UserRegistrationController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String showRegistrationController(){
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user")UserRegistrationDto userRegistrationDto){
+        userService.save(userRegistrationDto);
+        return "redirect:/registration?succes";
+    }
 }
